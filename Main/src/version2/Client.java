3027f8a7a28b3cabce1c4ea61d8216525cab5f65
nikopain <nicolas.curapil@alumnos.usm.cl>
2014-04-15 13:08:56 -0400

    avance

--- Main/src/version2/Client.java
+++ Main/src/version2/Client.java
@@ -6,10 +6,61 @@
 
 package version2;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 /**
  *
  * @author niko
  */
-public class Client {
+public class Client implements Runnable {
+    Socket Sock;
+    Scanner Input;
+    Scanner Send= new Scanner(System.in);
+    PrintWriter out;
+    public Client(Socket x) {
+        this.Sock=x;
+    }
+    public void run(){
+        try{
+            try{
+                Input=new Scanner(Sock.getInputStream());
+                out= new PrintWriter(Sock.getOutputStream());
+                out.flush();
+                CheckStream();
+                
+            }
+            finally{
+                Sock.close();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    private void CheckStream() {
+        while(true){
+            Receive();
+        }
+      
+    }
+
+    private void Receive() {
+        if(Input.hasNext()){
+            String mess= Input.nextLine();
+            if(mess.contains("#?!")){
+                String Temp1=mess.substring(3);
+                       Temp1=Temp1.replace("[", "");
+                       Temp1=Temp1.replace("]", "");
+                String[] CurrentUsers=Temp1.split(", ");
+                gui.Li_Online.setListData(CurrentUsers);
+                
+            }
+            else{
+                gui.T_Conversation.append(mess+"\n");
+            }
+        }
+    }
     
 }
